d2fc8955ef2c9b6a6629e73db5cdee2be645bd01
ErwanGF <erwangf@gmail.com>
2016-11-23 23:21:59 +0100

    Nettoyage code ( fait avec amour )

--- src/modules/data/Mongo.java
+++ src/modules/data/Mongo.java
@@ -111,9 +111,7 @@ public class Mongo {
      * @return le nombre d'éléments dans la collection (nombre de Tweets ici)
      */
     public long GetNbTweets() {
-        long nbTweet;
-        nbTweet = twitter.count();
-        return nbTweet;
+        return twitter.count();
     }
 
     /**
@@ -122,20 +120,21 @@ public class Mongo {
      * @return tous les Tweets de la collection dans une ArrayList de type Tweet
      */
     public ArrayList<Tweet> GetAllTweets() {
-        ArrayList<Tweet> tweetsV2 = new ArrayList<Tweet>();
-        ArrayList<Document> tweetsV1;
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        ArrayList<Document> documents;
 
-        tweetsV1 = twitter.find().into(new ArrayList<Document>());
-
-        Iterator<Document> iter = tweetsV1.iterator();
+        documents = twitter.find().into(new ArrayList<Document>());
+        Iterator<Document> iter = documents.iterator();
         while (iter.hasNext()) {
             Document doc = iter.next();
-            //System.out.println(doc);
-            Tweet t = new Tweet(doc.get("user").toString(), doc.get("TweetId").toString(), doc.get("username").toString(), new Timestamp(Long.parseLong(doc.get("date").toString())), doc.get("content").toString(), Integer.parseInt(doc.get("nbResponses").toString()), Integer.parseInt(doc.get("nbRetweets").toString()), Integer.parseInt(doc.get("nbLikes").toString()));
-
-            tweetsV2.add(t);
+            Tweet t = DocToTweet(doc);
+            tweets.add(t);
         }
-        return tweetsV2;
+
+
+
+
+        return tweets;
     }
 
     /**
@@ -158,7 +157,7 @@ public class Mongo {
      * @return Une ArrayList de Tweets contenant ce mot clé.
      */
     public ArrayList<Tweet> GetTweetsKeyWord(String keyWord) {
-        ArrayList<Tweet> tweetsV2 = new ArrayList<Tweet>();
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
         ArrayList<Document> tweetsV1 = new ArrayList<Document>();
 
         //	tweetsV1 = twitter.find({"content":keyWord}).into(new ArrayList<Document>());
@@ -172,21 +171,12 @@ public class Mongo {
                         .append("$options", "i"));
 
         MongoCursor<Document> cursor = twitter.find(regexQuery).iterator();
-        while (cursor.hasNext()) {
-            tweetsV1.add(cursor.next());
+        for (Document doc : twitter.find(regexQuery)) {
+            Tweet t = DocToTweet(doc);
+            tweets.add(t);
         }
-
-        Iterator<Document> iter = tweetsV1.iterator();
-        while (iter.hasNext()) {
-            Document doc = iter.next();
-            //System.out.println(doc);
-            Tweet t = new Tweet(doc.get("user").toString(), doc.get("TweetId").toString(), doc.get("username").toString(), new Timestamp(Long.parseLong(doc.get("date").toString())), doc.get("content").toString(), Integer.parseInt(doc.get("nbResponses").toString()), Integer.parseInt(doc.get("nbRetweets").toString()), Integer.parseInt(doc.get("nbLikes").toString()));
-
-            tweetsV2.add(t);
-        }
-        return tweetsV2;
+        return tweets;
     }
-
 
     /**
      *
@@ -215,6 +205,11 @@ public class Mongo {
         return tweets;
     }
 
+    /**
+     * Document to Tweet converter
+     * @param doc the document to convert
+     * @return a tweet
+     */
     private Tweet DocToTweet(Document doc) {
         return new Tweet(
                 doc.get("user").toString(),
@@ -227,7 +222,6 @@ public class Mongo {
                 Integer.parseInt(doc.get("nbLikes").toString()));
     }
 
-
     /**
      * Retourne un block de Tweet, de longueur limitée (100 normalement, voir Mongo.pageSize )
      *
@@ -235,21 +229,20 @@ public class Mongo {
      * @return Une ArrayList de Tweets
      */
     public ArrayList<Tweet> GetTweetsBlocks(int page) {
-        ArrayList<Tweet> tweetsV2 = new ArrayList<Tweet>();
-        ArrayList<Document> tweetsV1;
+        ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+        ArrayList<Document> documents;
 
 
         //pagination limitée en paramètre
-        tweetsV1 = twitter.find().skip((page - 1) * pageSize).limit(pageSize).into(new ArrayList<Document>());
+        documents = twitter.find().skip((page - 1) * pageSize).limit(pageSize).into(new ArrayList<Document>());
 
-        Iterator<Document> iter = tweetsV1.iterator();
+        Iterator<Document> iter = documents.iterator();
         while (iter.hasNext()) {
             Document doc = iter.next();
-            Tweet t = new Tweet(doc.get("user").toString(), doc.get("TweetId").toString(), doc.get("username").toString(), new Timestamp(Long.parseLong(doc.get("date").toString())), doc.get("content").toString(), Integer.parseInt(doc.get("nbResponses").toString()), Integer.parseInt(doc.get("nbRetweets").toString()), Integer.parseInt(doc.get("nbLikes").toString()));
-
-            tweetsV2.add(t);
+            Tweet t = DocToTweet(doc);
+            tweets.add(t);
         }
-        return tweetsV2;
+        return tweets;
     }
 
     /**
